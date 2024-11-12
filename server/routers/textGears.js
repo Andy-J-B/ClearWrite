80a3b271148212394348c712f7f98bd78a3da690
Andy-J-B <080.abae@gmail.com>
2024-11-11 21:30:00 -0800

    Add third feature for TextGears API

--- server/routers/textGears.js
+++ server/routers/textGears.js
@@ -80,4 +80,38 @@ router.post("/readability", async (req, res) => {
   }
 });
 
+// Summarizer
+router.post("/summarize", async (req, res) => {
+  // Get text to check readability
+  const text = req.body.text;
+
+  // Replace all spaces with a +
+  const addTexts = text.replace(/ /g, "+");
+
+  // Get apiKey from dotenv file
+  const apiKey = process.env.TextGears_API_KEY;
+  const url = `https://api.textgears.com/summarize?key=${apiKey}&language=en-GB&text=${addTexts}`;
+
+  try {
+    // Should not test too many times due to api usage limit
+    // Uncomment only for testing and production
+    // const data = await axios.get(url);
+    const summaries = data?.data?.response;
+
+    if (summaries?.keywords.length > 0) {
+      res.json({
+        originalText: text,
+        corrections: summaries?.summary,
+        all: summaries,
+      });
+    } else if (text.length == 0) {
+      res.json({ error: "No words present to evaluate." });
+    } else {
+      res.json({ error: "Essay is too short to evaluate." });
+    }
+  } catch (error) {
+    res.status(500).send(`Error: ${error}`);
+  }
+});
+
 module.exports = router;
