924252f97f2f27158bdf479e54d1ed73662966ee
Andy-J-B <080.abae@gmail.com>
2024-11-12 13:39:27 -0800

    File documentation

--- server/routers/textGears.js
+++ server/routers/textGears.js
@@ -1,3 +1,12 @@
+/*
+ *** textGears.js
+ ***
+ *** Description : API used for 3 features :
+ ***               Correcting grammatical mistakes and making suggested changes
+ ***               Evaluate the readability of the text
+ ***               Summarize the essay by looking for key points
+ */
+
 // Import necessary modules
 const express = require("express");
 const router = express.Router();
@@ -15,6 +24,8 @@ router.post("/correctGrammar", async (req, res) => {
 
   // Get apiKey from dotenv file
   const apiKey = process.env.TextGears_API_KEY;
+
+  // Server request url
   const url = `https://api.textgears.com/grammar?text=${addTexts}&language=en-GB&whitelist=&dictionary_id=&ai=1&key=${apiKey}`;
 
   try {
@@ -25,9 +36,11 @@ router.post("/correctGrammar", async (req, res) => {
     // Uncomment only for testing and production
     // const data = await axios.get(url);
 
+    // Extract errors
     const errors = data?.data?.response?.errors;
 
     if (errors.length > 0) {
+      // If there are grammatical errors
       res.json({
         originalText: text,
         corrections: errors.map((error) => ({
@@ -37,12 +50,14 @@ router.post("/correctGrammar", async (req, res) => {
         all: errors,
       });
     } else {
+      // If there are no grammatical errors
       res.json({
         originalText: text,
         corrections: [],
       });
     }
   } catch (error) {
+    // If there is an error
     res.status(500).send(`Error: ${error}`);
   }
 });
@@ -57,14 +72,19 @@ router.post("/readability", async (req, res) => {
 
   // Get apiKey from dotenv file
   const apiKey = process.env.TextGears_API_KEY;
+
+  // Server request url
   const url = `https://api.textgears.com/readbility?key=${apiKey}&text=${addTexts}`;
 
   try {
     // Should not test too many times due to api usage limit
     // Uncomment only for testing and production
     // const data = await axios.get(url);
+
+    // Extract statistics
     const statistics = data?.data?.response?.stats;
     if (statistics?.counters?.words > 30) {
+      // If there are enough words
       res.json({
         originalText: text,
         fleschKincaid: statistics?.fleschKincaid,
@@ -73,9 +93,11 @@ router.post("/readability", async (req, res) => {
         all: statistics,
       });
     } else {
+      // If there are too little words
       res.json({ error: "Essay is too short to evaluate." });
     }
   } catch (error) {
+    // Catch errors
     res.status(500).send(`Error: ${error}`);
   }
 });
@@ -90,28 +112,37 @@ router.post("/summarize", async (req, res) => {
 
   // Get apiKey from dotenv file
   const apiKey = process.env.TextGears_API_KEY;
+
+  // Server request url
   const url = `https://api.textgears.com/summarize?key=${apiKey}&language=en-GB&text=${addTexts}`;
 
   try {
     // Should not test too many times due to api usage limit
     // Uncomment only for testing and production
     // const data = await axios.get(url);
+
+    // Extract summaries
     const summaries = data?.data?.response;
 
     if (summaries?.keywords.length > 0) {
+      // If there are enough sentences
       res.json({
         originalText: text,
         corrections: summaries?.summary,
         all: summaries,
       });
     } else if (text.length == 0) {
+      // If there are no words at all
       res.json({ error: "No words present to evaluate." });
     } else {
+      // If the essay is too short
       res.json({ error: "Essay is too short to evaluate." });
     }
   } catch (error) {
+    // Catch error
     res.status(500).send(`Error: ${error}`);
   }
 });
 
+// Export router
 module.exports = router;
