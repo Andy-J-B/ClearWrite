ed758bcde3915a578422db9412384efd230aab43
Andy-J-B <080.abae@gmail.com>
2024-11-11 21:18:03 -0800

    Add second API feature : readability check using TextGearsAPI

--- server/routers/textGears.js
+++ server/routers/textGears.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // Load environmental variables
 require("dotenv").config();
 
+// Correct Grammatical issues
 router.post("/correctGrammar", async (req, res) => {
   // Get text to grammar check
   const text = req.body.text;
@@ -16,8 +17,6 @@ router.post("/correctGrammar", async (req, res) => {
   const apiKey = process.env.TextGears_API_KEY;
   const url = `https://api.textgears.com/grammar?text=${addTexts}&language=en-GB&whitelist=&dictionary_id=&ai=1&key=${apiKey}`;
 
-  console.log(url);
-
   try {
     // Codes from lines 24-42 is from
     // "https://gist.github.com/krishheii/415ccdf6ab9a6bb29d60bdcdbdb5e98c"
@@ -25,7 +24,9 @@ router.post("/correctGrammar", async (req, res) => {
     // Should not test too many times due to api usage limit
     // Uncomment only for testing and production
     // const data = await axios.get(url);
+
     const errors = data?.data?.response?.errors;
+
     if (errors.length > 0) {
       res.json({
         originalText: text,
@@ -40,6 +41,39 @@ router.post("/correctGrammar", async (req, res) => {
         originalText: text,
         corrections: [],
       });
+    }
+  } catch (error) {
+    res.status(500).send(`Error: ${error}`);
+  }
+});
+
+// Readability grading
+router.post("/readability", async (req, res) => {
+  // Get text to check readability
+  const text = req.body.text;
+
+  // Replace all spaces with a +
+  const addTexts = text.replace(/ /g, "+");
+
+  // Get apiKey from dotenv file
+  const apiKey = process.env.TextGears_API_KEY;
+  const url = `https://api.textgears.com/readbility?key=${apiKey}&text=${addTexts}`;
+
+  try {
+    // Should not test too many times due to api usage limit
+    // Uncomment only for testing and production
+    // const data = await axios.get(url);
+    const statistics = data?.data?.response?.stats;
+    if (statistics?.counters?.words > 30) {
+      res.json({
+        originalText: text,
+        fleschKincaid: statistics?.fleschKincaid,
+        emotion: statistics?.emotion,
+        counters: statistics?.counters,
+        all: statistics,
+      });
+    } else {
+      res.json({ error: "Essay is too short to evaluate." });
     }
   } catch (error) {
     res.status(500).send(`Error: ${error}`);
