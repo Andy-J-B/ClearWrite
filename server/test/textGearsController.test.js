ee6b5f27e11d37ff741767d6697cdbc52a6d59a3
Andy-J-B <080.abae@gmail.com>
2024-11-13 22:53:17 -0800

    install npm http mocks

--- server/test/textGearsController.test.js
+++ server/test/textGearsController.test.js
@@ -1,1 +1,26 @@
-const { CorrectGrammar } = require("../controller/textGearsController");
+const {
+  CorrectGrammar,
+  Readability,
+  Summarize,
+} = require("../controller/textGearsController");
+
+const httpMocks = require("node-mocks-http");
+
+describe("CorrectGrammar", () => {
+  test("Test grammatical checking and suggestions", () => {
+    const request = httpMocks.createRequest({
+      method: "GET",
+      url: "/some/url",
+    });
+
+    const response = httpMocks.createResponse();
+
+    sendSomeStuff(request, response, (err) => {
+      expect(err).toBeFalsy();
+    });
+
+    const { property } = JSON.parse(response._getData());
+
+    expect(property).toBe("someValue");
+  });
+});
