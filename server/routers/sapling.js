3c012a5eb1b6c19d9c142dc2686e7896fd7317de
Adityapal Waraich <asw18@sfu.ca>
2024-12-03 15:55:49 -0800

    Added the comments to the sapling.js

--- server/routers/sapling.js
+++ server/routers/sapling.js
@@ -1,10 +1,10 @@
 /*
- *** textGears.js
+ *** sapling.js
  ***
  *** Description : API used for 3 features :
- ***               Correcting grammatical mistakes and making suggested changes
- ***               Evaluate the readability of the text
- ***               Summarize the essay by looking for key points
+ ***               Rephrasing the text using different mapping like informal to formal, etc.
+ ***               Detecting the AI in the text
+ ***               Sentiment analysis of the text and provide the tone of the text
  */
 
 // Import necessary modules
@@ -21,8 +21,8 @@ const saplingController = require("../controller/saplingController");
 if (process.env.NODE_ENV === "test") {
   jest.mock("axios");
 }
-
-// Rephrasing
+// Features of the API
+// 1. Rephrasing
 router.post("/rephrase", saplingController.Rephrase);
 
 // 2. AI detection
