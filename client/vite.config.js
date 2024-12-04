36beeb942e021c4b514f7f52d7e1c03ce7999e6c
Andy-J-B <080.abae@gmail.com>
2024-12-04 10:18:41 -0800

    Revert vite.config.js:

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import eslintPlugin from "vite-plugin-eslint";
 
+// https://vite.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    eslintPlugin({
-      fix: true, // Automatically attempt to fix problems on save
-      include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"], // Add your source file patterns here
-    }),
-  ],
+  plugins: [react()],
 });
